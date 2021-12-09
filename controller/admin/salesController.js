a714fc9cb5b055dd79b2b70b4dafbabf07790aa1
Katherine Davenia <katherinedavenia24@gmail.com>
2021-12-09 10:20:41 +0700

    bug fix

--- controller/admin/salesController.js
+++ controller/admin/salesController.js
@@ -3,16 +3,21 @@ const Order_details = require('../../models/order_details');
 module.exports = {
     salesReport: async (req, res) => {
         try {
-            const sales = await Order_details.findAll({
-                attributes: [
-                    'id', 'quantity', 'price', 'medicineId'
-                ]
-            });
+            const sales = await Order_details.findAll();
             res.status(200).send(sales)
         } catch (err) {
             console.error(err.message);
             return res.status(500).send({ message: "Server error" });
         }
     },
+
+    revenueReport: async (req, res) => {
+        try {
+            res.status(200).send({ message: "tes doang okeee" })
+        } catch (err) {
+            console.error(err.message);
+            return res.status(500).send({ message: "Server error" });
+        }
+    }
 };
 
