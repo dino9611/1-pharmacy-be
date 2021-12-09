7ec113132943a70b88f393f1d52d556851d95941
Timotius Anrez <71978054+TimotiusAnrez@users.noreply.github.com>
2021-12-09 15:13:35 +0700

    Delete routes directory

--- routes/api/profile.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const profileRoutes = express.Router();
-const Profile = require('../../controller/userProfile/profileController');
-
-profileRoutes.get('/:id', Profile.getProfile);
-profileRoutes.put('/:id/info', Profile.editProfile);
-profileRoutes.post('/users', Profile.register);
-
-module.exports = profileRoutes;
