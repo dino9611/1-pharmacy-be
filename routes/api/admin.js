8091f8b073546e1492176749fb87d0983293c0e6
Katherine Davenia <katherinedavenia24@gmail.com>
2021-12-09 17:21:57 +0700

    PBS-31: View all ongoing transactions for user's page

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -2,12 +2,14 @@ const express = require('express');
 const adminRoutes = express.Router();
 const { dashboard } = require('../../controller/authentication/authController');
 const { salesReport, revenueReport } = require('../../controller/reportDatas/reportController');
-const { allTransactions } = require('../../controller/reportDatas/userTransactionController');
+const { allTransactions, ongoingTransactions } = require('../../controller/reportDatas/userTransactionController');
 const { verifyAdminToken } = require('../../middleware');
 
 adminRoutes.get('/dashboard', verifyAdminToken(), dashboard);
 adminRoutes.get('/sales', salesReport);
 adminRoutes.get('/revenue', revenueReport);
 adminRoutes.get('/allTransactions', allTransactions);
+adminRoutes.get('/ongoingTransactions', ongoingTransactions);
+
 
 module.exports = adminRoutes;
