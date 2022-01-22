9ba726d46c65871809b1249b8319be9240c131e7
Timotius Anrez <71978054+TimotiusAnrez@users.noreply.github.com>
2022-01-22 09:47:05 +0700

    Beta 2.2

--- controller/cart/Cart.js
+++ controller/cart/Cart.js
@@ -1,7 +1,8 @@
+const { Op } = require('sequelize');
+const { Cart_details } = require('../../models');
 const db = require('../../models');
 
 const Carts = db.Carts;
-const Users = db.Users;
 const Medicines = db.Medicines;
 
 class CartController {
@@ -33,13 +34,46 @@ class CartController {
 		//basic cart add function without auto decrement will await for update
 	}
 	static async getCart(req, res) {
-		const check = await Users.findOne({
+		const check = await Carts.findOne({
 			where: { id: req.params.id },
-			include: Carts,
+			include: Medicines,
 		});
 
-		console.log(check.Cart);
 		res.send(check);
+	}
+
+	static async editItemInCart(req, res) {
+		let update = await Cart_details.update(
+			{ quantity: req.body.quantity },
+			{
+				where: {
+					[Op.and]: [{ CartId: +req.params.id }, { MedicineId: req.body.id }],
+				},
+			},
+		);
+
+		let data = await Cart_details.findOne({
+			where: {
+				[Op.and]: [{ CartId: +req.params.id }, { MedicineId: req.body.id }],
+			},
+		});
+
+		console.log(data);
+
+		res.send(data);
+	}
+	static async deleteItemInCart(req, res) {
+		let destroy = await Cart_details.destroy({
+			where: {
+				[Op.and]: [{ CartId: +req.params.id }, { MedicineId: req.body.id }],
+			},
+		});
+		const data = await Carts.findOne({
+			where: { id: req.params.id },
+			include: Medicines,
+		});
+
+		res.send(data);
 	}
 }
 
